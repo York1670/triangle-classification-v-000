b782fe8ffa0b4b019c24be13421581b6855e7d05
Bobbi York <york1670@gmail.com>
2019-02-09 00:18:53 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ class Triangle
       :isosceles
     elsif @hyp != @opp && @opp != @adj && @hyp != @adj
       :scalene
-    elsif @hyp == 0 || @opp == 0 || @adj == 0
+    elsif @hyp == 0 && @opp == 0 && @adj == 0
       return TriangleError
     end
   end
